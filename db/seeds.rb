f1c1e111d89ff18fecf8de77571c661fe421a3ea
Mikey Esteban <michaelp.esteban@gmail.com>
2021-01-26 08:16:11 -0500

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+def grab_image_url
+  image_url = Faker::LoremFlickr.image
+  image_url
+end
+
+def grab_sentence
+  sentence = Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 10)
+  sentence
+end
+
+def grab_paragraph
+  paragraph = ''
+  rand(1...8).times do
+    paragraph += "#{grab_sentence} "
+  end
+  paragraph
+end
+
+def grab_blog
+  blog = ''
+  rand(2...6).times do
+    blog += "#{grab_paragraph}\n"
+  end
+  blog
+end
+
+blogs = Blog.create([
+  {
+    title: grab_sentence,
+    image_url: grab_image_url,
+    body: grab_blog
+  },
+  {
+    title: grab_sentence,
+    image_url: grab_image_url,
+    body: grab_blog
+  },
+  {
+    title: grab_sentence,
+    image_url: grab_image_url,
+    body: grab_blog
+  }
+])
