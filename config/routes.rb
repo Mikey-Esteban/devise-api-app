a2a1eb6feaf39dd5af9d22f8fae9a4df51adcca1
Mikey Esteban <michaelp.esteban@gmail.com>
2021-01-26 09:07:10 -0500

    fixed typo in routes, api is working

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :blog, param: :slug, except: [:new]
+      resources :blogs, param: :slug, except: [:new]
       resources :comments, only: [ :create, :destroy ]
     end
   end
